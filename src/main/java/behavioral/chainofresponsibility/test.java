09a4ab085bcda280eb1b1a2925ec4cc05251b797
freebird.sky4@gmail.com <sharmistha29>
2019-06-24 00:24:59 +0530

    behvdesign

--- src/main/java/behavioral/chainofresponsibility/test.java
+++ src/main/java/behavioral/chainofresponsibility/test.java
@@ -1,5 +1,6 @@
 package behavioral.chainofresponsibility;
 
+import java.math.BigDecimal;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,5 +16,9 @@ public class test {
         logger.finer("i am finer");
         logger.finest("i am finest");
 
+        /*Test*/
+        Request request = new Request(RequestType.CLIENTVISIT,new BigDecimal(100000000));
+        Handler director=new Director();VP vp = new VP();vp.setSuccessor(new CEO());director.setSuccessor(vp);
+        director.handleRequest(request);
     }
 }
